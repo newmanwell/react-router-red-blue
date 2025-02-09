74dbe80f3609a6ffb0035b03a17013aa7bacbf90
Shaun Newman <shaun.newman.p@gmail.com>
2025-02-09 11:50:31 -0500

    Added requited div to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 
 const App = () => {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-
-    </>
+    <div id="container">
+      <h1>Hello React Router!</h1>
+      <div id="navbar">{/* navigation here */}</div>
+      <div id="main-section">{/* routes here */}</div>
+    </div>
   )
 }
 
