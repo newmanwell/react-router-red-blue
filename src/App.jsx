04d6819f16fca0c906dd8d2613fcb9462407bdfe
Shaun Newman <shaun.newman.p@gmail.com>
2025-02-09 18:21:42 -0500

    home component added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import {Routes, Route, Link } from "react-router-dom";
 
 import Blue from "./Blue";
 import Red from "./Red";
+import Home from "./Home";
 
 const App = () => {
 
@@ -10,11 +11,13 @@ const App = () => {
       <div id="navbar">
         <Link to="/blue">Blue</Link>
         <Link to="/red">Red</Link>
+        <Link to="/">Home</Link>
       </div>
       <div id="main-section">
         <Routes>
           <Route path="/blue" element={<Blue />}></Route>
           <Route path="/red" element={<Red />}></Route>
+          <Route path="/" element={<Home />}></Route>
         </Routes>
       </div>
     </div>
